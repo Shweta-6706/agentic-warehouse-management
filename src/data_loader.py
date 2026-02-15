3709fdc10c1a8ae295fdb0fc6885afc05fb1a2e4
Shweta-6706 <2405101010069@paruluniversity.ac.in>
2026-02-15 20:30:03 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import ast
+
+def load_data(file_path):
+    df = pd.read_csv("data/warehouse_data.csv")
+
+    # Convert historical_demand string back to list
+    df["historical_demand"] = df["historical_demand"].apply(ast.literal_eval)
+
+    return df
+
 
