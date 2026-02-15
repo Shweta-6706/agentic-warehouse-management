b094d9bc2f44790431a6cd62a31875cf3db3414f
Shweta-6706 <2405101010069@paruluniversity.ac.in>
2026-02-15 20:33:50 +0530

    Update test_optimization.py

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -1,1 +1,11 @@
+from src.optimization import calculate_eoq
+
+def test_eoq_basic():
+    annual_demand = 1000
+    ordering_cost = 50
+    holding_cost = 2
+
+    eoq = calculate_eoq(annual_demand, ordering_cost, holding_cost)
+
+    assert eoq > 0
 
